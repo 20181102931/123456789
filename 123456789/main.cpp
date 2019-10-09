87fa74ada5c26d23a4080ffcf9f97b2f73d77626
20181102931 <18647197975@163.com>
2019-10-09 11:49:33 +0800

    123
    123

--- 123456789/main.cpp
+++ 123456789/main.cpp
@@ -1,35 +1,129 @@
-//
-//  main.cpp
-//  123456789
-//
-//  Created by s20181102931 on 2019/8/27.
-//  Copyright © 2019 s20181102931. All rights reserved.
-//
-
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 100
 using namespace std;
-int main()
+struct Athlete
 {
-    int n=10,m=0;
-    int a[10];
-    for(int i=0;i<n;i++)
+    int school;//学校
+    int item;//项目编号
+    int rank; //名次
+    int score; //成绩
+    char name[20]; //姓名
+    char sex; //性别
+};
+typedef struct
+{
+    Athlete elem[MAXSIZE];
+    int last;
+}Seqlist;
+int sum(int m,int w)//计算总人数
+{
+    int Sum=0;
+    for(int i=1;i<=(m+w);i++)
     {
-        cin>>a[i];
+        if(i%2==1)
+            Sum=Sum+5;
+        else
+            Sum=Sum+3;
     }
-    for(int j=0;j<n-1;j++)
+    return Sum;
+}
+void change(Athlete* e)
+{
+    if(e->item%2==0)
     {
-        for(int j=0;j<n;j++)
-      {
-        if(a[j]>a[j+1])
+        switch(e->rank)
         {
-            m=a[j+1];
-            a[j+1]=a[j];
-            a[j]=m;
+            case 1:
+                e->score=5;
+                break;
+            case 2:
+                e->score=3;
+                break;
+            case 3:
+                e->score=2;
+                break;
         }
-      }
     }
-    for(int k=0;k<n;k++)
+    if(e->item%2==1)
     {
-        cout<<a[k]<<endl;
+        switch(e->rank)
+        {
+            case 1:
+                e->score=7;
+                break;
+            case 2:
+                e->score=5;
+                break;
+            case 3:
+                e->score=3;
+                break;
+            case 4:
+                e->score=2;
+                break;
+            case 5:
+                e->score=1;
+                break;
+        }
     }
 }
+void input( Seqlist* L,int m,int w)
+{
+    L->last=sum(m,w);
+    cout<<"请输入项目编号 运动员姓名 性别（w/m） 学校  名次"<<endl;
+    for(int i=1;i<=L->last;i++)
+    {
+        cin>>L->elem[i].item>>L->elem[i].name>>L->elem[i].sex >>L->elem[i].school>>L->elem[i].rank;
+        change(&L->elem[i]);
+    }
+}
+void display(Seqlist* L,int n)
+{
+    int j,i;
+    int s=0,ms=0,ws=0;
+    cout<<"打印成绩单和成绩报表："<<endl;
+    for(j=1;j<=n;j++) //学校数目
+    {
+        ws=0;
+        ms=0;
+        s=0;
+        cout<<"学校 姓名 性别 项目名次 分数"<<endl;
+        for(i=1;i<=L->last;i++)
+        {
+            if(L->elem[i].school==j)
+            {
+                cout<<" "<<L->elem[i].school<<" "
+                         <<L->elem[i].name<<" "
+                         <<L->elem[i].sex<<" "
+                         <<L->elem[i].item<<" "
+                         <<L->elem[i].rank<<" "
+                         <<L->elem[i].score <<endl;
+                if(L->elem[i].sex=='w')
+                {
+                    ws+=L->elem[i].score;
+                }
+                if(L->elem[i].sex=='m')
+                {
+                    ms+=L->elem[i].score;
+                }
+            }
+            s=ws+ms;
+        }
+        cout<<j<<"学校的团体总分是；"<<s<<endl;
+        cout<<j<<"学校的女子团体总分是："<<ws<<endl;
+        cout<<j<<"学校的男子团体总分是；"<<ms<<endl;
+        cout<<endl;
+    }
+}
+int main()
+{
+    Seqlist L;
+    int n,m,w;
+    cout<<"请输入学校个数 男子项目 女子项目的个数："<<endl;
+    cin>>n>>m>>w;
+    input(&L,m,w); //赋值操作
+    display(&L,n);
+    return 0;
+}
+
